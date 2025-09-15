8e6ac82a4cedf64354695739e158245817c220d5
JoaoDorneles <108199016+JoaoDorneles@users.noreply.github.com>
2025-09-15 14:25:54 -0300

    Update Retangulo.java
    implementando métodos de Retângulo

--- Retangulo.java
+++ Retangulo.java
@@ -1,3 +1,21 @@
-public class Retangulo{
+public class Retangulo {
+    public String getTipoFigura(){
+        return "Retângulo";
+    }
 
+    public double calcularPerimetro(double base, double altura){
+        double perimetro;
+
+        perimetro = (base+altura)*2;
+        
+        return perimetro;
+    }
+
+    public double calcularArea(double base, double altura){
+        double area;
+        
+        area = base*altura;
+
+        return area;
+    }
 }
