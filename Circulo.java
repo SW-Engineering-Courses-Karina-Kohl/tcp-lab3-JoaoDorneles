77cf407f7967f5731aafa787f098829ee2c3139b
JoaoDorneles <108199016+JoaoDorneles@users.noreply.github.com>
2025-09-15 14:25:15 -0300

    Update Circulo.java
    implementando métodos de Círculo

--- Circulo.java
+++ Circulo.java
@@ -1,3 +1,22 @@
-public class Circulo{
+public class Circulo {
 
+    public String getTipoFigura(){
+        return "Círculo";
+    }
+
+    public double calcularPerimetro(double raio){
+        double perimetro;
+
+        perimetro = 2*Math.PI*raio;
+
+        return perimetro;
+    }
+
+    public double calcularArea(double raio){
+        double area;
+        
+        area = Math.PI*(Math.pow(raio,2));
+
+        return area;
+    }
 }
