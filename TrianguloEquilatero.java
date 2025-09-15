843b96809d79e5e5792884d1d3b64372b602b791
JoaoDorneles <108199016+JoaoDorneles@users.noreply.github.com>
2025-09-15 14:26:52 -0300

    Update TrianguloEquilatero.java
    implementando métodos de Triângulo Equilátero

--- TrianguloEquilatero.java
+++ TrianguloEquilatero.java
@@ -1,3 +1,21 @@
-public class TrianguloEquilatero{
+public class TrianguloEquilatero {
+    public String getTipoFigura(){
+        return "Triângulo";
+    }
 
+    public double calcularPerimetro(double lado){
+        double perimetro;
+        
+        perimetro = lado*3;
+
+        return perimetro;
+    }
+
+    public double calcularArea(double lado){
+        double area;
+
+        area = (((Math.pow(lado,2))*(Math.sqrt(3))))/4;
+ 
+        return area;
+    }
 }
