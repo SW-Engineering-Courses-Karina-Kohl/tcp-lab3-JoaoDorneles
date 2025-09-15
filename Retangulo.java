b3a8127e2a7e2a4470d9e617419dc99bdb344e51
JoaoDorneles <108199016+JoaoDorneles@users.noreply.github.com>
2025-09-15 15:12:07 -0300

    Update Retangulo.java
    adição de métodos de Retângulo

--- Retangulo.java
+++ Retangulo.java
@@ -1,21 +1,53 @@
-public class Retangulo {
+public class Retangulo extends FiguraGeometrica{
+    
+    double largura;
+    double altura;
+    
+    public Retangulo(String cor, double largura, double altura){
+        super(cor);
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    public double getlargura(){
+        return largura;
+    }
+
+    public void setlargura(double largura){
+        this.largura = largura;
+    }
+
+    public double getaltura(){
+        return altura;
+    }
+
+    public void setaltura(double altura){
+        this.altura = altura;
+    }
+
     public String getTipoFigura(){
         return "Retângulo";
     }
 
-    public double calcularPerimetro(double base, double altura){
+    public double calcularPerimetro(double largura, double altura){
         double perimetro;
 
-        perimetro = (base+altura)*2;
+        perimetro = (largura+altura)*2;
         
         return perimetro;
     }
 
-    public double calcularArea(double base, double altura){
+    public double calcularArea(double largura, double altura){
         double area;
         
-        area = base*altura;
+        area = largura*altura;
 
         return area;
+    }
+
+    public void getDetalhes(){
+        getInfo();
+        System.out.println("Largura: "+largura);
+        System.out.println("Altura: "+altura);
     }
 }
