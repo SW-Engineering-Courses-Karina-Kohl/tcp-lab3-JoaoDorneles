7975b1ac0afe33bf44d023e5bd3eb299a3656c64
JoaoDorneles <108199016+JoaoDorneles@users.noreply.github.com>
2025-09-15 15:11:24 -0300

    Update FiguraGeometrica.java
    implementação FiguraGeometrica

--- FiguraGeometrica.java
+++ FiguraGeometrica.java
@@ -1,5 +1,31 @@
-import java.util.Random;
-
 public abstract class FiguraGeometrica{
+
+public String cor;
+public int id;
+public static int totalFiguras;
+
+public FiguraGeometrica(String cor){
+    id = totalFiguras;
+    totalFiguras++;
+}
+
+public void setCor(String cor){
+    this.cor = cor;
+}
+
+public String getCor(){
+    return cor;
+}
+
+public static int getTotalFiguras(){
+    return totalFiguras;
+}
+
+public void getInfo(){
+    System.out.println("Id: "+id);
+    System.out.println("Cor: "+cor);
+}
+
+public abstract void getDetalhes();
 
 }
